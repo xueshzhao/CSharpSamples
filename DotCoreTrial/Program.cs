b1cbe62b1d6f600ff09db93692c66d28b4127f73
Xue Shan Zhao <xueshzhao@163.com>
2018-07-31 17:46:54 +0800

    commit all changes, which is mainly to add solution and project files

---
+++ DotCoreTrial/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotCoreTrial.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Add for Git operation");
+        }
+    }
+}
