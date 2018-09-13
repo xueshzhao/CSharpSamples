04ef83cb8c3cd4d89e27c20694f784ff53b78161
Xue Shan Zhao <xueshzhao@163.com>
2018-09-14 06:51:02 +0800

    Add file operation

--- DotCoreTrial/Program.cs
+++ DotCoreTrial/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DotCoreTrial.TrialSample;
 
 namespace DotCoreTrial.HelloWorld
 {
@@ -9,6 +10,8 @@ namespace DotCoreTrial.HelloWorld
             Console.WriteLine("Hello World!");
             Console.WriteLine("Add for Git operation trial");
             Console.WriteLine("now we add a new branch");
+            FileOperationTrial sample = new FileOperationTrial();
+            sample.generateFile(@"d:/1.txt");
         }
     }
 }
